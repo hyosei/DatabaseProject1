645730bd57625cf00296cf4c88cd69bd8265dc8b
hyosei <jjung1995@yonsei.ac.kr>
2017-12-07 03:31:00 +0900

    Add files via upload

--- cctv/admin.py
+++ cctv/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Manager)
+admin.site.register(CCTV)
+admin.site.register(Shoot)
+admin.site.register(Shoot_space)
+admin.site.register(Sequence)
+admin.site.register(Neighborhood)
+
+
+
+
 
 # Register your models here.
